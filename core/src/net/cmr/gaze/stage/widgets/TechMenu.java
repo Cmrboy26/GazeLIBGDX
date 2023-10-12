70de7be91e45eb208aa46e07a1416d7cee55ae55
Cmrboy26 <cmrboy26@gmail.com>
2023-10-11 22:16:18 -0500

    Tech Widget Progress

--- core/src/net/cmr/gaze/stage/widgets/TechMenu.java
+++ core/src/net/cmr/gaze/stage/widgets/TechMenu.java
@@ -11,6 +11,7 @@ import com.badlogic.gdx.scenes.scene2d.ui.Image;
 import com.badlogic.gdx.scenes.scene2d.ui.ImageButton;
 import com.badlogic.gdx.scenes.scene2d.ui.ScrollPane;
 import com.badlogic.gdx.scenes.scene2d.ui.ScrollPane.ScrollPaneStyle;
+import com.badlogic.gdx.scenes.scene2d.ui.Table;
 import com.badlogic.gdx.scenes.scene2d.ui.WidgetGroup;
 import com.badlogic.gdx.scenes.scene2d.utils.TextureRegionDrawable;
 import com.badlogic.gdx.utils.JsonReader;
@@ -24,6 +25,8 @@ public class TechMenu extends WidgetGroup {
 
     ButtonGroup<ImageButton> categoryButtonGroup;
     ImageButton confirmButton;
+    ResearchTree researchTree;
+    ScrollPane researchPanel;
 
     public TechMenu(Gaze game) {
         this.game = game;
@@ -67,52 +70,140 @@ public class TechMenu extends WidgetGroup {
         });
         addActor(confirmButton);
 
-        ScrollPaneStyle style = new ScrollPaneStyle();
-        ScrollPane researchPanel = new ScrollPane(null, style);
-        researchPanel.setBounds(57*2+24*2, 36*2+21*2, 168*2, 81*2);
+        WidgetGroup group = new WidgetGroup();
+        group.setBounds(0, 0, 168*2, 81*2*5);
+        Image icon = new Image(new TextureRegionDrawable(game.getSprite(GameMenuIcon.CRAFTING_ICON)));
+        icon.setBounds(168-16, 10, 16*2, 16*2);
+        group.addActor(icon);
+        Image icon2 = new Image(new TextureRegionDrawable(game.getSprite(GameMenuIcon.INVENTORY_ICON)));
+        icon2.setBounds(168-16, 10+50, 16*2, 16*2);
+        group.addActor(icon2);
+        Image icon3 = new Image(new TextureRegionDrawable(game.getSprite(GameMenuIcon.QUESTS_ICON)));
+        icon3.setBounds(168-16, 10+50+130, 16*2, 16*2);
+        group.addActor(icon3);
 
+        Table table = new Table();
+        table.add(group).width(168*2).height(81*4);
+
+        ScrollPaneStyle style = new ScrollPaneStyle();
+        researchPanel = new ScrollPane(table, style);
+        researchPanel.setBounds(57*2+24*2, 36*2+21*2, 168*2, 81*2);
+        researchPanel.layout();
+        researchPanel.setScrollPercentY(1f);
+        setResearchTree("{\r\n" + //
+						"\t\"comment\":\"the id will be appened to the namespace and all research nodes will append 'parent-id' to that\",\r\n" + //
+						"\t\"namespace\":\"gaze\",\r\n" + //
+						"\t\"root\":{\r\n" + //
+						"\t\t\"name\":\"Machinery\",\r\n" + //
+						"\t\t\"description\":\"The basis of all industry\",\r\n" + //
+						"\t\t\"icon\":\"machinery\",\r\n" + //
+						"\t\t\"id\":\"machinery\"\r\n" + //
+						"\t},\r\n" + //
+						"\t\"comment2\":\"the parent research is ALWAYS a requirement\",\r\n" + //
+						"\t\"researchNodes\":[\r\n" + //
+						"\t\t{\t\r\n" + //
+						"\t\t\t\"name\":\"Gears\",\r\n" + //
+						"\t\t\t\"description\":\"Gears and gadgets\",\r\n" + //
+						"\t\t\t\"icon\":\"gearSymbol\",\r\n" + //
+						"\t\t\t\"id\":\"gears\",\r\n" + //
+						"\t\t\t\"parent-id\":\"root\",\r\n" + //
+						"\t\t\t\"requirements\": [\r\n" + //
+						"\t\t\t\t\"ITEM/IRON_INGOT/5\"\r\n" + //
+						"\t\t\t],\r\n" + //
+						"\t\t\t\"rewards\": []\r\n" + //
+						"\t\t},\r\n" + //
+						"\t\t{\r\n" + //
+						"\t\t\t\"name\":\"Electricity\",\r\n" + //
+						"\t\t\t\"description\":\"POWER\",\r\n" + //
+						"\t\t\t\"icon\":\"electricSymbol\",\r\n" + //
+						"\t\t\t\"id\":\"electricity\",\r\n" + //
+						"\t\t\t\"parent-id\":\"gears\",\r\n" + //
+						"\t\t\t\"requirements\":[\r\n" + //
+						"\t\t\t\t\"ITEM/WOOD_PICKAXE/1\",\r\n" + //
+						"\t\t\t\t\"RESEARCH/gaze:machinery.electricity\"\r\n" + //
+						"\t\t\t],\r\n" + //
+						"\t\t\t\"rewards\":[\r\n" + //
+						"\t\t\t\t\"XP/MINING/10\",\r\n" + //
+						"\t\t\t\t\"ITEM/WOOD_PICKAXE/1\"\r\n" + //
+						"\t\t\t]\r\n" + //
+						"\t\t},\r\n" + //
+						"\t\t{\r\n" + //
+						"\t\t\t\"name\":\"Power Transportation\",\r\n" + //
+						"\t\t\t\"description\":\"Move around electricity\",\r\n" + //
+						"\t\t\t\"icon\":\"powerPole\",\r\n" + //
+						"\t\t\t\"id\":\"powerTransport\",\r\n" + //
+						"\t\t\t\"parent-id\":\"electricity\",\r\n" + //
+						"\t\t\t\"requirements\":[\r\n" + //
+						"\t\t\t\t\"ITEM/COPPER_WIRE/10\"\r\n" + //
+						"\t\t\t],\r\n" + //
+						"\t\t\t\"rewards\":[\r\n" + //
+						"\t\t\t\t\"XP/MINING/15\",\r\n" + //
+						"\t\t\t\t\"ITEM/POWER_POLE/10\"\r\n" + //
+						"\t\t\t]\r\n" + //
+						"\t\t}\r\n" + //
+						"\t]\r\n" + //
+						"}");
         addActor(researchPanel);
     } 
+
+    @Override
+    public void act(float delta) {
+        super.act(delta);
+        if(!isVisible()) {
+            getStage().setScrollFocus(null);
+        }
+    }
+
+    public void setResearchTree(String string) {
+        try {
+            researchTree = ResearchTree.deriveResearchGraph(string);
+		} catch (InvalidObjectException e) {
+			e.printStackTrace();
+		}
+    }
     
     /*
         
         {
-            "comment": "the id will be appened to the namespace and all research nodes will append 'parent-id' to that",
-            "namespace": "gaze",
-            "root": {
-                "name": "Machinery",
-                "description": "The basis of all industry",
-                "id": "machinery"
+            "comment":"the id will be appened to the namespace and all research nodes will append 'parent-id' to that",
+            "namespace":"gaze",
+            "root":{
+                "name":"Machinery",
+                "description":"The basis of all industry",
+                "id":"machinery"
             },
-            "comment2": "the parent research is ALWAYS a requirement",
-            "researchNodes": [{
-                "name": "Electricity",
-                "description": "POWER",
-                "icon": "electricSymbol",
-                "id": "electricity",
-                "parent-id": "root",
-                "requirements": [
-                    "ITEM|WOOD_PICKAXE:1",
-                    "RESEARCH|gaze:machinery.electricity"
-                ],
-                "rewards": [
-                    "XP|MINING|10",
-                    "ITEM|WOOD_PICKAXE:1"
-                ]
-            }, {
-                "name": "Power Transportation",
-                "description": "Move around electricity",
-                "icon": "powerPole",
-                "id": "powerTransport",
-                "parent-id": "electricity",
-                "requirements": [
-                    "ITEM|COPPER_WIRE:10"
-                ],
-                "rewards": [
-                    "XP|MINING:15",
-                    "ITEM|POWER_POLE:10"
-                ]
-            }]
+            "comment2":"the parent research is ALWAYS a requirement",
+            "researchNodes":[
+                {
+                    "name":"Electricity",
+                    "description":"POWER",
+                    "icon":"electricSymbol",
+                    "id":"electricity",
+                    "parent-id":"root",
+                    "requirements":[
+                        "ITEM/WOOD_PICKAXE/1",
+                        "RESEARCH/gaze:machinery.electricity"
+                    ],
+                    "rewards":[
+                        "XP/MINING/10",
+                        "ITEM/WOOD_PICKAXE/1"
+                    ]
+                },
+                {
+                    "name":"Power Transportation",
+                    "description":"Move around electricity",
+                    "icon":"powerPole",
+                    "id":"powerTransport",
+                    "parent-id":"electricity",
+                    "requirements":[
+                        "ITEM/COPPER_WIRE/10"
+                    ],
+                    "rewards":[
+                        "XP/MINING/15",
+                        "ITEM/POWER_POLE/10"
+                    ]
+                }
+            ]
         }
 
          */
@@ -137,7 +228,8 @@ public class TechMenu extends WidgetGroup {
             graph.root = new ResearchVertex(graph, mainJSONObject.get("root"));
 
             for(JsonValue value : mainJSONObject.get("researchNodes")) {
-                graph.createVertex(value);
+                ResearchVertex vertex = graph.createVertex(value);
+                System.out.println(graph.getUniversalID(vertex.parent) + "\t->\t" +graph.getUniversalID(vertex));
             }
 
             return graph;
@@ -150,12 +242,21 @@ public class TechMenu extends WidgetGroup {
         }
 
         public ResearchVertex getVertex(String id) {
+            if(id.equals("root"))
+                return root;
             return researchNodes.get(id);
         }
 
         public void addChild(ResearchVertex parent, ResearchVertex child) {
             parent.children.add(child);
             child.parent = parent;
+        }
+
+        public String getUniversalID(ResearchVertex vertex) {
+            if(vertex.equals(root)) {
+                return namespace + ":" + root.getID();
+            }
+            return namespace + ":" + root.getID() + "." + vertex.getID();
         }
 
     }
@@ -177,12 +278,12 @@ public class TechMenu extends WidgetGroup {
                 "id": "electricity",
                 "parent-id": "root",
                 "requirements": [
-                    "ITEM|WOOD_PICKAXE:1",
-                    "RESEARCH|gaze:machinery.electricity"
+                    "ITEM/WOOD_PICKAXE/1",
+                    "RESEARCH/gaze:machinery.electricity"
                 ],
                 "rewards": [
-                    "XP|MINING|10",
-                    "ITEM|WOOD_PICKAXE:1"
+                    "XP/MINING/10",
+                    "ITEM/WOOD_PICKAXE/1"
                 ]
             }
 
@@ -197,38 +298,48 @@ public class TechMenu extends WidgetGroup {
             this.description = json.getString("description");
             this.icon = json.getString("icon");
             this.ID = json.getString("id");
-            String parentID = json.getString("parent-id");
             if(tree.root == null) {
                 tree.root = this;
                 parent = null;
             } else {
+                String parentID = json.getString("parent-id");
                 parent = tree.getVertex(parentID);
                 tree.addChild(parent, this);
-            }
-            // read requirements
-            JsonValue requirements = json.get("requirements");
-            this.requirements = new Requirement[requirements.size];
-            for(int i = 0; i < requirements.size; i++) {
-                this.requirements[i] = new Requirement(requirements.getString(i));
+                // read requirements
+                JsonValue requirements = json.get("requirements");
+                this.requirements = new Requirement[requirements.size];
+                for(int i = 0; i < requirements.size; i++) {
+                    this.requirements[i] = new Requirement(requirements.getString(i));
+                }
             }
         }
 
         static class Requirement {
             enum RequirementType {
                 ITEM, RESEARCH, LEVEL;
+                public static RequirementType get(String name) {
+                    for(RequirementType type : values()) {
+                        if(type.name().equals(name))
+                            return type;
+                    }
+                    return null;
+                }
             } 
             RequirementType category;
+            Object type, value;
             // category |type     | value
             // ITEM     |WOOD_AXE | 1
-            // RESEARCH |gaze:machinery.electricity | 1
+            // RESEARCH |gaze:machinery.electricity 
             // LEVEL    |MINING   | 10
-            Object type, value;
 
             public Requirement(String requirementString) {
-                String[] split = requirementString.split("|");
-                category = RequirementType.valueOf(split[0]);
+                String[] split = requirementString.split("/");
+                // print everything in split
+                category = RequirementType.get(split[0]);
                 type = split[1];
-                value = split[2];
+                if(split.length > 2) {
+                    value = split[2];
+                }
             }
         }
 
--- desktop/src/net/cmr/gaze/DesktopLauncher.java
+++ desktop/src/net/cmr/gaze/DesktopLauncher.java
@@ -1,7 +1,12 @@
 package net.cmr.gaze;
 
+import java.io.InvalidObjectException;
+
 import com.badlogic.gdx.backends.lwjgl3.Lwjgl3Application;
 import com.badlogic.gdx.backends.lwjgl3.Lwjgl3ApplicationConfiguration;
+
+import net.cmr.gaze.stage.widgets.TechMenu;
+import net.cmr.gaze.stage.widgets.TechMenu.ResearchTree;
 
 // Please note that on macOS your application needs to be started with the -XstartOnFirstThread JVM argument
 public class DesktopLauncher {
@@ -12,5 +17,64 @@ public class DesktopLauncher {
 		config.setTitle("Gaze");
 		config.setWindowedMode(640, 360);
 		new Lwjgl3Application(new Gaze() {}, config);
+
+		/*try {
+			ResearchTree.deriveResearchGraph(
+				"{\r\n" + //
+						"\t\"comment\":\"the id will be appened to the namespace and all research nodes will append 'parent-id' to that\",\r\n" + //
+						"\t\"namespace\":\"gaze\",\r\n" + //
+						"\t\"root\":{\r\n" + //
+						"\t\t\"name\":\"Machinery\",\r\n" + //
+						"\t\t\"description\":\"The basis of all industry\",\r\n" + //
+						"\t\t\"icon\":\"machinery\",\r\n" + //
+						"\t\t\"id\":\"machinery\"\r\n" + //
+						"\t},\r\n" + //
+						"\t\"comment2\":\"the parent research is ALWAYS a requirement\",\r\n" + //
+						"\t\"researchNodes\":[\r\n" + //
+						"\t\t{\t\r\n" + //
+						"\t\t\t\"name\":\"Gears\",\r\n" + //
+						"\t\t\t\"description\":\"Gears and gadgets\",\r\n" + //
+						"\t\t\t\"icon\":\"gearSymbol\",\r\n" + //
+						"\t\t\t\"id\":\"gears\",\r\n" + //
+						"\t\t\t\"parent-id\":\"root\",\r\n" + //
+						"\t\t\t\"requirements\": [\r\n" + //
+						"\t\t\t\t\"ITEM/IRON_INGOT/5\"\r\n" + //
+						"\t\t\t],\r\n" + //
+						"\t\t\t\"rewards\": []\r\n" + //
+						"\t\t},\r\n" + //
+						"\t\t{\r\n" + //
+						"\t\t\t\"name\":\"Electricity\",\r\n" + //
+						"\t\t\t\"description\":\"POWER\",\r\n" + //
+						"\t\t\t\"icon\":\"electricSymbol\",\r\n" + //
+						"\t\t\t\"id\":\"electricity\",\r\n" + //
+						"\t\t\t\"parent-id\":\"gears\",\r\n" + //
+						"\t\t\t\"requirements\":[\r\n" + //
+						"\t\t\t\t\"ITEM/WOOD_PICKAXE/1\",\r\n" + //
+						"\t\t\t\t\"RESEARCH/gaze:machinery.electricity\"\r\n" + //
+						"\t\t\t],\r\n" + //
+						"\t\t\t\"rewards\":[\r\n" + //
+						"\t\t\t\t\"XP/MINING/10\",\r\n" + //
+						"\t\t\t\t\"ITEM/WOOD_PICKAXE/1\"\r\n" + //
+						"\t\t\t]\r\n" + //
+						"\t\t},\r\n" + //
+						"\t\t{\r\n" + //
+						"\t\t\t\"name\":\"Power Transportation\",\r\n" + //
+						"\t\t\t\"description\":\"Move around electricity\",\r\n" + //
+						"\t\t\t\"icon\":\"powerPole\",\r\n" + //
+						"\t\t\t\"id\":\"powerTransport\",\r\n" + //
+						"\t\t\t\"parent-id\":\"electricity\",\r\n" + //
+						"\t\t\t\"requirements\":[\r\n" + //
+						"\t\t\t\t\"ITEM/COPPER_WIRE/10\"\r\n" + //
+						"\t\t\t],\r\n" + //
+						"\t\t\t\"rewards\":[\r\n" + //
+						"\t\t\t\t\"XP/MINING/15\",\r\n" + //
+						"\t\t\t\t\"ITEM/POWER_POLE/10\"\r\n" + //
+						"\t\t\t]\r\n" + //
+						"\t\t}\r\n" + //
+						"\t]\r\n" + //
+						"}");
+		} catch (InvalidObjectException e) {
+			e.printStackTrace();
+		}*/
 	}
 }
